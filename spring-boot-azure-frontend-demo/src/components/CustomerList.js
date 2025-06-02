c0ebba573ddfb693b99f4b4b0423d0e0e29b385c
Maifo_Manyane <155142836+maifokeletsi@users.noreply.github.com>
2025-06-02 17:18:14 +0200

    Allow CORS Origin For Frontend

---
+++ spring-boot-azure-frontend-demo/src/components/CustomerList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function CustomerList() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://manyane-gkbpcvcncxhhfhhq.southafricanorth-01.azurewebsites.net/api/getAllCustomers')
+      .then(res => setCustomers(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>All Customers</h2>
+      <ul>
+        {customers.map(customer => (
+          <li key={customer.id}>
+            {customer.fullName} - {customer.email} - {customer.city}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CustomerList;
